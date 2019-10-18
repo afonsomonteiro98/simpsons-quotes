a4aee6e79e95e2024c1019dde5c7e6f2d9f9eb4e
Afonso <afonsom98@gmail.com>
2019-10-18 10:44:55 +0100

    Quest

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Quotes from "./Quotes"
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.handleButtonClick=this.handleButtonClick.bind(this);
+    this.state={ working:true };
+  }
+  
+  // This is the wrong way of calling the set state method. Inside set state we should use a function and not a object!
+  // handleButtonClick () {
+  //   this.setState({working: !this.state.working})
+  // }
+  
+  handleButtonClick () {
+    this.setState((state) => {
+      return {working: !state.working};
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className={this.state.working ? "App-logo" : "App-logo-transform"} alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
+          <button onClick={this.handleButtonClick}> Badjoras </button>
+        </header>
+        <Quotes />
+      </div>
+    );
+  }
 }
 
 export default App;
